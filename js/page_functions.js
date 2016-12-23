dedc3bc607f4bba72108d979acc6cf976ee9971b
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2016-12-23 03:41:15 -0300

    Adjusts lenghts with viewport

--- js/page_functions.js
+++ js/page_functions.js
@@ -1,6 +1,6 @@
-$('#video-present').click(function(){
+$('#play-presentation').click(function(){
     video = '<iframe src="'+ $('#video-present').attr('data-video') +'"></iframe>';
-    text = '<div></div>'
+    text = '<div><button id="play-presentation" class="btn btn-default btn-lg btn-block" style="float: right; top: 39vh;"><i class="fa fa-stop-circle fa-4x blackiconcolor" aria-hidden="true"></i></button></div>'
     $('#first-text').replaceWith(text);
     $('#video-present').replaceWith(video);
 });
