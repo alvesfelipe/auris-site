e96aebdd6cfe2f22ca56fba75d37973b3639443d
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2017-01-03 05:01:37 -0300

    Implements navbar scroll down functions

--- js/page_functions.js
+++ js/page_functions.js
@@ -12,19 +12,74 @@ $('body').on('click', '#stop-presentation', function(){
     location.href = 'http://192.168.0.8:8080';
 });
 
-var i = 0;
+var i = 0, sec_sum = 0;
 var sections = [($('#home').height() - $('#head').height()), 
-                ($('#description').height())
+                ($('#description').height()),
+                ($('#objectives-auris').height()),
+                ($('#experiments').height()),
+                ($('#github').height()),
+                ($('#team').height()),
+                ($('#publications').height()),
+                ($('#partners').height()),
+                ($('#contact').height()),
                 ];
+
 
 $('#scroll-down').click(function () {
     
-    if ((sections[i-1]) != null) { 
-        $('html, body').animate({scrollTop: sections[i] + sections[i-1]}, 'slow');
+    if ((sections[i-1]) != null && sections[i] != null) {
+        sec_sum += sections[i];
+        alert(sec_sum);
+        $('html, body').animate({scrollTop: sec_sum}, 'slow');
     }else{
+        sec_sum = sections[i];
+        alert(sec_sum);
         $('html, body').animate({scrollTop: sections[i]}, 'slow');
     }
 
     i++;
     return false;
 });
+
+function summ(i) {
+    var aux = 0;
+    var s = sections[aux];
+    
+    while(aux < i) {
+        aux++;
+        s += sections[aux];
+    }
+
+    return s;
+}
+
+function project(id) {
+    $('html, body').animate({scrollTop: sections[0]}, 'slow');
+    return false;
+}
+
+function experiment(id) {
+
+    $('html, body').animate({scrollTop: summ(2)}, 'slow');
+    return false;
+}
+
+function git(id) {
+    $('html, body').animate({scrollTop: summ(3)}, 'slow');
+    return false;
+}
+
+function team(id) {
+    $('html, body').animate({scrollTop: summ(4)}, 'slow');
+    return false;
+}
+
+function partners(id) {
+    $('html, body').animate({scrollTop: summ(6)}, 'slow');
+    return false;
+}
+
+function contact(id) {
+    $('html, body').animate({scrollTop: summ(7)}, 'slow');
+    return false;
+}
