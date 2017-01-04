38c31f7c21fb72c646bff8bb8b17aa420bead30c
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2017-01-04 06:24:53 -0300

    Adjusts fonts

--- js/page_functions.js
+++ js/page_functions.js
@@ -1,3 +1,10 @@
+// $(function(){ 
+//      var navMain = $("#nav-main");
+//      navMain.on("click", "a", null, function () {
+//          navMain.collapse('hide');
+//      });
+//  });
+
 $('#play-presentation').click(function(){
     video = '<iframe id="video-present" src="https://www.youtube.com/embed/NsOnkL8hog0?autoplay=1&showinfo=0&controls=0"></iframe>';
     text = '<div id="first-text"><button id="stop-presentation" class="btn btn-default btn-lg btn-block"><i class="fa fa-stop-circle fa-5 iconcolor" aria-hidden="true"></i></button></div>';
