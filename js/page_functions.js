b17cc2510487048897ede573c6cfbd74a4937010
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2017-01-20 10:18:34 -0300

    Implements reload of home section by click

--- js/page_functions.js
+++ js/page_functions.js
@@ -4,7 +4,7 @@ $('body').on('click', '#logo-auris', function(){
 });
 
 // video functions
-$('#play-presentation').click(function(){
+$('body').on('click', '#play-presentation', function(){
     video = '<iframe id="video-present" src="https://www.youtube.com/embed/NsOnkL8hog0?autoplay=1&showinfo=0&controls=0&version=3&loop=1&playlist=NsOnkL8hog0"></iframe>';
     text = '<div id="first-text"><button id="stop-presentation" class="btn btn-default btn-lg btn-block"><i class="fa fa-stop-circle fa-5 iconcolor" aria-hidden="true"></i></button></div>';
     button = '<button></button>'
@@ -15,7 +15,7 @@ $('#play-presentation').click(function(){
 });
 
 $('body').on('click', '#stop-presentation', function(){
-    location.href = 'https://alvesfelipe.github.io/auris-site/';
+    $('#home').load(document.URL +  ' #home');
 });
 
 // navbar functions
